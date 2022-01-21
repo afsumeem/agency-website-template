aaeade27b8a8a281df3481f2ac3a68da497d54df
afsumeem <afsumeem@gmail.com>
2022-01-21 22:30:07 +0600

    responsive done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 const navigationButton = document.querySelector('.navigation-button');
 let navigationBarOpen = false;
+
+const navbarMenu = document.querySelector("nav ul");
+const navbarLinks = document.querySelectorAll("navbar ul a");
+
+// navigationButton.addEventListener("click", navbarTogglerClick);
+
+// function navbarTogglerClick() {
+
+// }
+
+
 navigationButton.addEventListener('click', () => {
     if (!navigationBarOpen) {
         navigationButton.classList.add('open');
+        // navigationButton.classList.toggle("open-navbar-toggler");
+        navbarMenu.classList.toggle("open");
         navigationBarOpen = true;
     } else {
         navigationButton.classList.remove('open');
         navigationBarOpen = false;
+        navbarMenu.classList.toggle("open");
     }
 });
