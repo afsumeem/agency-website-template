ceea7decaf389d8b4549b1a2da07de1c0a4ad559
afsumeem <afsumeem@gmail.com>
2022-01-21 22:31:56 +0600

    update

--- js/main.js
+++ js/main.js
@@ -4,17 +4,9 @@ let navigationBarOpen = false;
 const navbarMenu = document.querySelector("nav ul");
 const navbarLinks = document.querySelectorAll("navbar ul a");
 
-// navigationButton.addEventListener("click", navbarTogglerClick);
-
-// function navbarTogglerClick() {
-
-// }
-
-
 navigationButton.addEventListener('click', () => {
     if (!navigationBarOpen) {
         navigationButton.classList.add('open');
-        // navigationButton.classList.toggle("open-navbar-toggler");
         navbarMenu.classList.toggle("open");
         navigationBarOpen = true;
     } else {
